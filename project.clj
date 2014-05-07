e5c6c510f93a97bde903a0b8ac269a626b1cd184
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-07 20:33:13 +0800

    Update.

--- project.clj
+++ project.clj
@@ -3,15 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2202"]
+                 ;;[org.clojure/clojurescript "0.0-2156"]
                  [enlive "1.1.5"]
                  [compojure "1.1.6"]
                  [http-kit "2.1.18"]]
   :profiles {:uberjar {:aot :all}}
   :target-path "target/%s"
   :plugins [[lein-cljsbuild "1.0.3"]
-            [com.cemerick/austin "0.1.4"]]
+            [com.cemerick/austin "0.1.5-SNAPSHOT"]]
 
   :source-paths ["src/clj" "src/cljs"]
   :main ^:skip-aot reversi.server
@@ -19,13 +20,12 @@
   :cljsbuild {
               :builds [{
                         :source-paths ["src/cljs"]
-                        :externs ["resources/public/phaser.min.js"]
                         :compiler {
                                    :output-to "resources/public/reversi-phaser.js"
                                    :optimizations :whitespace
-                                   ;; :optimizations :advanced
-                                   ;; :optimizations :simple
                                    :pretty-print true
+                                   ;; :optimizations :advanced
+                                   ;; :externs ["resources/public/phaser.min.js"]
                                    }}]}
   :repl-options {:init-ns reversi.server}
   )
--- src/clj/reversi/server.clj
+++ src/clj/reversi/server.clj
@@ -3,9 +3,9 @@
   (:require [cemerick.austin.repls :refer (browser-connected-repl-js)]
             [net.cgrand.enlive-html :as enlive]
             [compojure.route :refer (resources)]
-            [compojure.core :refer (GET defroutes)]  
+            [compojure.core :refer (GET defroutes)]
             org.httpkit.server
-            [clojure.java.io :as io]))                                 
+            [clojure.java.io :as io]))
 
 (enlive/deftemplate page
   (io/resource "index.html")
@@ -14,7 +14,7 @@
             (enlive/html [:script (browser-connected-repl-js)])))
 
 (defroutes site
-  (resources "/") 
+  (resources "/")
   (GET "/*" req (page)))
 
 (defn run
@@ -23,7 +23,20 @@
     (org.httpkit.server/run-server #'site {:port 8081 :join? false}))
   server)
 
-(defn -main 
+(defn -main
   "run http server"
   [& args]
   (run))
+
+(def repl-env)
+
+(defn init-cljs-repl []
+  (->> (reset! cemerick.austin.repls/browser-repl-env
+               (cemerick.austin/repl-env))
+       constantly
+       (alter-var-root #'repl-env))
+  ;; return nil to avoid printing and OOM
+  nil)
+
+(defn cljs-repl-run []
+  (cemerick.austin.repls/cljs-repl reversi.server/repl-env))
